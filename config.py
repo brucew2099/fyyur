0178f492ae0d0fe08419a68ad39ed4c9a52fb26d
Alan Kwok <alankwok1@gmail.com>
2020-09-01 00:10:31 -0400

    Updated config file

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import dbconfig as cfg
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,8 +9,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+USER = cfg.postgressql['user']
+PASSWD = cfg.postgressql['passwd']
+HOST = cfg.postgressql['host']
+PORT = cfg.postgressql['port']
+DB = cfg.postgressql['db']
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:<<password>>,@localhost:5432/fyyurapp'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWD}@{HOST}:{PORT}/{DB}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
